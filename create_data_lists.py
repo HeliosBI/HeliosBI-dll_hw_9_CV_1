d2443d2141b1962dff5346630d1422232ce64f70
HeliosBI <aan-123@yandex.ru>
2021-03-19 22:29:20 +0500

    ssd for bccd

---
+++ create_data_lists.py
@@ -0,0 +1,5 @@
+from utils import create_data_lists
+
+if __name__ == '__main__':
+    create_data_lists(voc07_path='D:/Backup/Курс Нетология/Глубокое обучение/9 CV/dll_hw_9_CV1/SSD_for_BCCD/BCCD',
+                      output_folder='D:/Backup/Курс Нетология/Глубокое обучение/9 CV/dll_hw_9_CV1/SSD_for_BCCD/Out')
